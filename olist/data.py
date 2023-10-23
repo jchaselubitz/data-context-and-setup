2127704094db2cbfe1c062dc7c865e9f7a105eb3
jchaselubitz <jchaselubitz@gmail.com>
2023-10-23 15:54:09 +0200

    added data

--- olist/data.py
+++ olist/data.py
@@ -1,23 +1,28 @@
 import os
 import pandas as pd
 
+"""
+This function returns a Python dict.
+Its keys should be 'sellers', 'orders', 'order_items' etc...
+Its values should be pandas.DataFrames loaded from csv files
+"""
+
 
 class Olist:
     def get_data(self):
-        """
-        This function returns a Python dict.
-        Its keys should be 'sellers', 'orders', 'order_items' etc...
-        Its values should be pandas.DataFrames loaded from csv files
-        """
-        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
-            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
-            # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
-        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+        csv_path = os.path.join(os.environ["PYTHONPATH"][:-1], "data", "csv")
+        file_names = os.listdir(csv_path)
+        data = {}
+        for name in file_names:
+            if name[:2] != ".k":
+                key = (
+                    name.replace("olist_", "")
+                    .replace(".csv", "")
+                    .replace("_dataset", "")
+                )
+
+                data[key] = pd.read_csv(os.path.join(csv_path, name))
+        return data
 
     def ping(self):
-        """
-        You call ping I print pong.
-        """
         print("pong")
